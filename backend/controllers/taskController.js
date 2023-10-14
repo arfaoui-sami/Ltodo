2fb01c672fcc752576ef63d4ba1cc4a2615dd80e
arfaoui-sami <sami.arfaoui@tek-up.de>
2023-10-14 17:45:01 +0100

    last commit

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -27,11 +27,15 @@ const sendMail = (email, subject, title, description) => {
         }
     });
 }
+// Add a new task
 const addTask = async (req, res) => {
     const { title, description } = req.body;
     const userId = req.user.id;
+    // Find the user corresponding to the ID
     const user = await userModel.find({_id: userId});
+     // Create a new task with the provided data
     const newTask = new taskModel({ title, description, terminée: false, userId })
+   // Save the new task to the database
     newTask.save()
         .then(() => {
             sendMail(user[0].email, "Task Added", title, description)
@@ -39,21 +43,25 @@ const addTask = async (req, res) => {
         })
         .catch((error) => {
             return (
+                // Handle errors and return a response in case of failure
                 res.status(500).json({ message: error.message })
             )
         }
         )
 }
+// Remove a task
 const removeTask = (req, res) => {
-    
+// Retrieve the ID of the task to be deleted from the request
     const { id } = req.body;
     console.log("id: ", id);
+    // Find the task by its ID and delete it
     taskModel.findByIdAndDelete(id)
         .then(() => res.status(200).json({ message: "Task deleted successfully" }))
         .catch((error) => res.status(501).json({ message: error.message }))
 }
-
+// Get tasks for the current user
 const getTask = (req, res) => {
+      // Find and return all tasks belonging to the current user
     taskModel.find({ userId: req.user.id })
         .then((data) => res.status(200).json(data))
         .catch((error) => res.status(501).json({ message: error.message }))
